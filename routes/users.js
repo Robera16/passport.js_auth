22b39719a1cc75bca2ead31cf6c4f2ba6af2e5e4
robera16 <roberaworknehrobwork@gmail.com>
2021-10-27 00:08:11 +0300

    added dashboard and route protecting feature

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
 const bcrypt = require("bcryptjs");
-
+const passport = require("passport");
 // Login page
 router.get("/login", (req, res) => res.render("login"));
 
@@ -83,4 +83,19 @@ router.post("/register", (req, res) => {
   }
 });
 
+// Login Handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+// Logout Handle
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
+});
 module.exports = router;
